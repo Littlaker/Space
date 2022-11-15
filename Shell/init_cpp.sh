8017f1d491561bc19cb02006d2f629bd5e800b6b
adobtz <adobtz@163.com>
2022-11-15 22:02:08 +0800

    Add Shell

---
+++ Shell/init_cpp.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+sudo apt-get update
+sudo apt-get install -y gcc
+sudo apt-get install -y g++
+sudo apt-get install -y gdb
+
+
+sudo apt-get install -y cmake
+sudo apt-get install -y cppcheck
+sudo apt-get install -y valgrind
+sudo apt-get install -y cpplint
---
+++ Shell/init_tool.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+sudo apt-get update
+
+sudo apt-get install -y git
+sudo apt-get install -y vim
+sudo apt-get install -y curl
+sudo apt-get install -y wget
+sudo apt-get install -y jq
